3e3d3a011b7a36b5b596d67b3a0e4c940968844c
Matthew Erwin <matthewderwin23@gmail.com>
2023-10-20 01:48:53 -0500

    Async storage edits

--- App/frontend/src/components/ProfileImage.js
+++ App/frontend/src/components/ProfileImage.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Image } from 'react-native';
 import { useSelector } from 'react-redux';
+import defaultImage from '../../assets/360default.png'
 
 const ProfileImage = ({ width, height }) => {
     const user = useSelector(state => state.user);
     return (
         <Image
-            source={user.profilePic ? { uri: user.profilePic } : null}
+            source={user.profilePic ? { uri: user.profilePic } : defaultImage}
             style={{ width: width, height: height }}
         />
     );
